bb9a8f59e0b94b9a9ce787aad210ce0c86023bf4
atakriti <anwart256@gmail.com>
2022-04-10 15:07:58 +0200

    onclick nav done

---
+++ src/javaScript/main.js
@@ -0,0 +1,35 @@
+let disney = () => {
+    let disneyList = document.getElementById("drop-list-disney")
+    if (disneyList.style.display === "none") {
+        disneyList.style.display = "block"
+    } else {
+        disneyList.style.display = "none"
+    }
+}
+// ==========main
+// let main = () => {
+//     let disneyList = document.getElementById("drop-list-disney")
+//     if (disneyList.style.display === "block") {
+//         disneyList.style.display = "none"
+//     } else {
+//         disneyList.style.display = "none"
+//     }
+// }
+// ========= filme
+let disneyFilme = () => {
+    let filmeList = document.getElementById("drop-list-filme")
+    if (filmeList.style.display === "none") {
+        filmeList.style.display = "block"
+    } else {
+        filmeList.style.display = "none"
+    }
+}
+// ============ parks
+let disneyParks = () => {
+    let parksList = document.getElementById("drop-list-parks")
+    if (parksList.style.display === "none") {
+        parksList.style.display = "block"
+    } else {
+        parksList.style.display = "none"
+    }
+}
